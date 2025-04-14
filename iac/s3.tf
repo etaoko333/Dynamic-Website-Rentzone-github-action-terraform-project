22c86b4f761a832c98a5a3e644a72bf83a06f778
etaoko333 <131201117+etaoko333@users.noreply.github.com>
2025-04-14 12:37:23 +0100

    Update s3.tf

--- iac/s3.tf
+++ iac/s3.tf
@@ -1,4 +1,7 @@
-#create s3 bucket
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = var.bucketname
+terraform {
+  backend "s3" {
+    bucket = "devopstechproject"    # Your specified S3 bucket name
+    key    = "terraform/state/terraform.tfstate"  # Path where the state file will be stored
+    region = "us-west-1"            # AWS region (replace with the appropriate region if necessary)
+  }
 }
